2a1a06a0345eda18c18322af63136ce1dda4aa80
Jeff Lehman <jlehman@huntington.edu>
2026-02-27 08:45:16 -0500

    Refactor comments for clarity in p6.py
    Updated comments for clarity and consistency throughout the code.

--- problem_sets/P6_Lists/p6.py
+++ problem_sets/P6_Lists/p6.py
@@ -1,36 +1,36 @@
 # p6.py
 # spring 2026
 # j.l. lehman
-# purpose of this assignment is to gain an understanding of how to 
+# The purpose of this assignment is to gain an understanding of how to 
 # create and use lists
 
 from random import *
 
 # *** lists ***
 
-#1 create list to hold 15 items all set to zero
+#1 Create a list to hold 15 items, all set to zero
 num = []
 
 print(num)
 print()
 
-#2 use a for loop to set all items in list equal to 2026
+#2 Use a for loop to set all items in the list equal to 2026
 
 print(num)
 print()
 
-#3 using a for loop add 100 to all items in list
+#3 Using a for loop, add 100 to all items in the list
 
 print(num)
 print()
 
-#4 using a for loop, set all items in list to a random number
+#4 Using a for loop, set all items in the list to a random number
 # in the range 2026 <= n <= 2099
 
 print(num)
 print()
 
-#5 display the smallest and largest values from the list
+#5 Display the smallest and largest values from the list
 
 small = -1
 
@@ -42,7 +42,7 @@ big = -1
 print("max:", big)
 print()
 
-#6 create a function called avg that returns the average value of
+#6 Create a function called avg that returns the average value of
 # all items in the list.
 
 def avg(temp):
@@ -52,14 +52,13 @@ def avg(temp):
 print("avg:", avg(num))
 print()
 
-#7 sort your list
+#7 Sort your list
 
 print("after sorting")
-
 print(num)
 print()
 
-#8 create a function called countRange that takes a list and a min and max
+#8 Create a function called countRange that takes a list and a min and max
 # range and returns a count of the number of list items that are in the
 # range count = number of items between m and n inclusive ie. m <= item <= n
 
@@ -87,43 +86,53 @@ verse = "Trust in the Lord with all your heart and lean not on your own understa
 citation = "Proverbs 3:5"
 words = []
 
-#10 use the split() method to store the words from the verse in a list called words
+#10 Use the split() method to store the words from the verse in a list called words
+
 print( words )
 print()
 
 #11 Use slice ie. [x:y] to display the first four words in the list (should work with any list)
+
 print()
 
 #12 Use slice ie. [x:y] to display the last three words (should work for any list)
+
 print()
 
-#13 Add the citation to the words list
-words.append( citation )
+#13 Add the citation to the end of the words list using append
+
 print( words )
 print()
 
 #14 Remove the citation from the end of the word list
+
 print( words )
 print()
 
-#15 use the IN operator to display all of the words
+#15 Use a while loop to display all of the words in the list
 print()
 
-#16 use a while loop to display all of the words in the list
+#16 Use a for loop with an index ie. words[i] to display all words in the list
 print()
 
-#17 use a for loop with an index ie. words[i] to display all words in the list
+#17 Use the IN operator to display all of the words
 print()
 
 #18 display all of the words in reverse order
 print()
 
-#19 remove the word "own" fromm the word list
+#19 Remove the word "own" from the word list
 print( words )
 print()
 
-#20 replace the word "lean" with "rely" in the word list
+#20 Replace the word "lean" with "rely" in the word list
 print( words )
+print()
+
+#21 Use the join operator to take each word and recreate the verse, storing as a string in variable verse_v2
+verse_v2 = ""
+
+print( verse_v2 )
 print()
 
 # -- end --
